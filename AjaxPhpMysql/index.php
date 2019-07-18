c771430a7189ab3b4428468655c77be1623ee265
huynhthaihung <huynhthaihung2765@gmail.com>
2019-07-18 10:29:13 +0700

    create demo

---
+++ AjaxPhpMysql/index.php
@@ -0,0 +1,25 @@
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Tìm Kiếm Live với Ajax</title>
+  <meta charset="UTF-8">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
+<script src="action.js "></script>
+ </head>
+ <body>
+  <div class="container">
+   <br />
+   <h2 align="center">Ajax Live Data Search using Jquery PHP MySql</h2><br />
+   <div class="form-group">
+    <div class="input-group">
+     <span class="input-group-addon">Search</span>
+     <input type="text" name="search_text" id="search_text" placeholder="Search by Customer Details" class="form-control" />
+    </div>
+   </div>
+   <br />
+   <div id="result"></div>
+  </div>
+ </body>
+</html>
